c8d4e1d89cf37e0544c39296a98d559702b01ed8
rajesh-askeva <askevarajesh@gmail.com>
2026-09-22 13:42:43 +0530

    Unused and Unwanted Script removed

--- frontend/src/api/superadminApi.js
+++
@@ -1,4 +0,0 @@
-import api from '../services/api';
-export const superadminApi = {
-  getCommandCenterData: () => api.get('/superadmin/command-center'),
-};
--- frontend/src/constants/masterItemNames.js
+++ frontend/src/constants/masterItemNames.js
@@ -1,1 +1,11 @@
-export const MASTER_ITEM_NAME_OPTIONS = [];
+export const MASTER_ITEM_NAME_OPTIONS = [
+  'Domain Purchase',
+  'Hosting',
+  'SSL Certificate',
+  'Email Services',
+  'Maintenance',
+  'Development',
+  'SEO Package',
+  'Social Media Marketing',
+  'Other'
+];
